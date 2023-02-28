e9b58b15585f39b24baaa84399507458e494d6e7
mitukou1109 <mitukou1109@gmail.com>
2023-02-28 21:00:00 +0900

    発射機構の状態をrvizに表示するようにした

--- tsuten_behavior/src/behavior_visualizer.cpp
+++ tsuten_behavior/src/behavior_visualizer.cpp
@@ -1,10 +1,13 @@
 #include <ros/ros.h>
 #include <ros/package.h>
+#include <jsk_rviz_plugins/OverlayText.h>
+#include <std_msgs/Bool.h>
 #include <tf2/utils.h>
 #include <tf2_ros/transform_listener.h>
 #include <visualization_msgs/MarkerArray.h>
 
 #include <tsuten_behavior/constants.hpp>
+#include <tsuten_mechanism/solenoid_valve_controller.hpp>
 
 namespace tsuten_behavior
 {
@@ -13,28 +16,97 @@ namespace tsuten_behavior
   public:
     BehaviorVisualizer()
     {
+      initializeShooterStates();
+
+      ros::NodeHandle behavior_server_nh("behavior_server");
+
+      for (auto &shooter_name_pair : SHOOTER_NAMES)
+      {
+        auto &shooter_id = shooter_name_pair.first;
+        auto &shooter_name = shooter_name_pair.second;
+
+        auto shooter_state_sub = behavior_server_nh.subscribe<std_msgs::Bool>(
+            shooter_name + "/state", 2,
+            boost::bind(&BehaviorVisualizer::setShooterState, this, shooter_id, _1));
+
+        shooter_state_subs_.insert({shooter_id, shooter_state_sub});
+      }
+
       ros::NodeHandle pnh("~");
 
       pnh.param("global_frame", global_frame_, std::string("map"));
       pnh.param("table_markers_publish_rate", table_markers_publish_rate_, 10.0);
 
+      shooter_state_text_pub_ =
+          pnh.advertise<jsk_rviz_plugins::OverlayText>("shooter_state_text", 10, true);
+
       table_markers_pub_ =
-          pnh.advertise<visualization_msgs::MarkerArray>("table_markers", 10, true);
+          pnh.advertise<visualization_msgs::MarkerArray>("table_markers", 10);
+
+      displayShooterStates();
+
+      createTableMarkers();
 
       publish_table_markers_timer_ =
           pnh.createTimer(ros::Rate(table_markers_publish_rate_),
                           &BehaviorVisualizer::publishTableMarkers, this);
-
-      createTableMarkers();
     }
 
   private:
+    using ValveState = tsuten_mechanism::SolenoidValveController::State;
+
+    static const std::unordered_map<ValveState, std::string> SHOOTER_STATE_TEXTS;
+
     static const std::unordered_map<TableID, std::array<float, 4>>
         TABLE_MARKER_COLORS;
 
+    void initializeShooterStates()
+    {
+      for (auto &shooter_name_pair : SHOOTER_NAMES)
+      {
+        auto &shooter_id = shooter_name_pair.first;
+
+        shooter_states_.emplace(shooter_id, ValveState::OFF);
+      }
+
+      shooter_state_text_.action = jsk_rviz_plugins::OverlayText::ADD;
+      shooter_state_text_.width = 280;
+      shooter_state_text_.height = 120;
+      shooter_state_text_.left = 10;
+      shooter_state_text_.top = 10;
+      shooter_state_text_.bg_color.r = 0;
+      shooter_state_text_.bg_color.g = 0;
+      shooter_state_text_.bg_color.b = 0;
+      shooter_state_text_.bg_color.a = 0.4;
+      shooter_state_text_.fg_color.r = 0.1;
+      shooter_state_text_.fg_color.g = 1.0;
+      shooter_state_text_.fg_color.b = 1.0;
+      shooter_state_text_.fg_color.a = 0.8;
+      shooter_state_text_.line_width = 1;
+      shooter_state_text_.text_size = 12;
+      shooter_state_text_.font = "Ubuntu";
+    }
+
+    void displayShooterStates()
+    {
+      std::string shooter_state_text;
+      for (auto &shooter_state_pair : shooter_states_)
+      {
+        auto &shooter_id = shooter_state_pair.first;
+        auto &shooter_state = shooter_state_pair.second;
+
+        shooter_state_text +=
+            SHOOTER_TEXTS.at(shooter_id) + ": " + SHOOTER_STATE_TEXTS.at(shooter_state) + "\n";
+      }
+
+      shooter_state_text_.text = shooter_state_text;
+
+      shooter_state_text_pub_.publish(shooter_state_text_);
+    }
+
     void createTableMarkers()
     {
-      static const std::string table_mesh_resource_dir =
+      const std::string table_mesh_resource_dir =
           ros::package::getPath("tsuten_description") + "/meshes";
 
       for (auto &table_name_pair : TABLE_NAMES)
@@ -64,6 +136,13 @@ namespace tsuten_behavior
 
         table_markers_.insert({table_id, table_marker});
       }
+    }
+
+    void setShooterState(const ShooterID &shooter_id, const std_msgs::Bool::ConstPtr &msg)
+    {
+      shooter_states_.at(shooter_id) = static_cast<ValveState>(msg->data);
+
+      displayShooterStates();
     }
 
     void publishTableMarkers(const ros::TimerEvent &event)
@@ -110,6 +189,14 @@ namespace tsuten_behavior
       }
     }
 
+    ros::Publisher shooter_state_text_pub_;
+
+    std::unordered_map<ShooterID, ros::Subscriber> shooter_state_subs_;
+
+    std::unordered_map<ShooterID, ValveState> shooter_states_;
+
+    jsk_rviz_plugins::OverlayText shooter_state_text_;
+
     ros::Publisher table_markers_pub_;
 
     ros::Timer publish_table_markers_timer_;
@@ -121,12 +208,17 @@ namespace tsuten_behavior
     double table_markers_publish_rate_;
   };
 
+  const std::unordered_map<BehaviorVisualizer::ValveState, std::string>
+      BehaviorVisualizer::SHOOTER_STATE_TEXTS =
+          {{ValveState::OFF, "<span style=\"color : blue;\">OFF</span>"},
+           {ValveState::ON, "<span style=\"color : red;\">ON</span> "}};
+
   const std::unordered_map<TableID, std::array<float, 4>>
-      BehaviorVisualizer::TABLE_MARKER_COLORS = {
-          {TableID::DUAL_TABLE, {1.0, 0.0, 0.0, 1.0}},
-          {TableID::MOVABLE_TABLE_1200, {1.0, 1.0, 0.0, 1.0}},
-          {TableID::MOVABLE_TABLE_1500, {1.0, 1.0, 0.0, 1.0}},
-          {TableID::MOVABLE_TABLE_1800, {1.0, 1.0, 0.0, 1.0}}};
+      BehaviorVisualizer::TABLE_MARKER_COLORS =
+          {{TableID::DUAL_TABLE, {1.0, 0.0, 0.0, 1.0}},
+           {TableID::MOVABLE_TABLE_1200, {1.0, 1.0, 0.0, 1.0}},
+           {TableID::MOVABLE_TABLE_1500, {1.0, 1.0, 0.0, 1.0}},
+           {TableID::MOVABLE_TABLE_1800, {1.0, 1.0, 0.0, 1.0}}};
 } // namespace tsuten_behavior
 
 int main(int argc, char **argv)
