1694c691a6603ca768a18f879c6b67cf0dd52ea0
mitukou1109 <mitukou1109@gmail.com>
2023-03-06 23:59:39 +0900

    移動制御をライブラリ化

---
+++ tsuten_navigation/include/tsuten_navigation/navigation_handler.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <ros/ros.h>
+#include <actionlib/client/simple_action_client.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <tf2/utils.h>
+
+namespace tsuten_navigation
+{
+  class NavigationHandler
+  {
+  public:
+    NavigationHandler();
+
+    bool waitForMoveBaseActionServer(const ros::Duration &timeout);
+
+    NavigationHandler &startNavigation(const tf2::Stamped<tf2::Transform> &goal_pose);
+
+    NavigationHandler &stopNavigation();
+
+    NavigationHandler &waitForNavigationToComplete();
+
+    NavigationHandler &commandVelocityToChassis(
+        const tf2::Vector3 &cmd_vel_linear,
+        const tf2::Vector3 &cmd_vel_angular = {0.0, 0.0, 0.0});
+
+    NavigationHandler &stopChassis();
+
+    bool isConnectedToMoveBaseActionServer();
+
+    bool isNavigationInProgress();
+
+  private:
+    actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> move_base_action_client_;
+
+    bool is_navigation_in_progress_;
+
+    ros::Publisher cmd_vel_pub_;
+  };
+}
