015f94643636eec850948ff4e0097854a99cee4a
mitukou1109 <mitukou1109@gmail.com>
2023-07-31 11:10:39 +0900

    amclを使わずに位置補正できるようにした

--- tsuten_navigation/src/odom_tf_publisher.cpp
+++ tsuten_navigation/src/odom_tf_publisher.cpp
@@ -1,6 +1,9 @@
 #include <ros/ros.h>
 #include <nav_msgs/Odometry.h>
+#include <tf2/utils.h>
 #include <tf2_ros/transform_broadcaster.h>
+
+#include <tsuten_msgs/CorrectRobotPosition.h>
 
 namespace tsuten_navigation
 {
@@ -11,6 +14,9 @@ namespace tsuten_navigation
     {
       ros::NodeHandle nh(""), pnh("~");
 
+      pnh.param("global_frame", global_frame_, std::string("map"));
+      pnh.param("odom_frame", odom_frame_, std::string("odom"));
+      pnh.param("robot_base_frame", robot_base_frame_, std::string("base_link"));
       pnh.param("odom_topic", odom_topic_, std::string("odom"));
       pnh.param("publish_rate", publish_rate_, 10.0);
 
@@ -18,37 +24,64 @@ namespace tsuten_navigation
 
       timer_ = nh.createTimer(ros::Rate(publish_rate_), &OdomTFPublisher::publishOdomTF, this);
 
-      odom_.pose.pose.orientation.w = 1.0;
+      correct_robot_position_service_server_ = nh.advertiseService(
+          "correct_robot_position", &OdomTFPublisher::correctRobotPosition, this);
+
+      map_tf_.setIdentity();
+      odom_tf_.setIdentity();
     }
 
   private:
     void publishOdomTF(const ros::TimerEvent &event)
     {
-      geometry_msgs::TransformStamped odom_tf;
+      geometry_msgs::TransformStamped map_tf_msg;
+      map_tf_msg.header.stamp = ros::Time::now();
+      map_tf_msg.header.frame_id = global_frame_;
+      map_tf_msg.child_frame_id = odom_frame_;
+      map_tf_msg.transform = tf2::toMsg(map_tf_);
 
-      odom_tf.header.stamp = ros::Time::now();
-      odom_tf.header.frame_id = "odom";
-      odom_tf.child_frame_id = "base_link";
-      odom_tf.transform.translation.x = odom_.pose.pose.position.x;
-      odom_tf.transform.translation.y = odom_.pose.pose.position.y;
-      odom_tf.transform.translation.z = odom_.pose.pose.position.z;
-      odom_tf.transform.rotation = odom_.pose.pose.orientation;
+      tf_broadcaster_.sendTransform(map_tf_msg);
 
-      tf_broadcaster_.sendTransform(odom_tf);
+      geometry_msgs::TransformStamped odom_tf_msg;
+      odom_tf_msg.header.stamp = ros::Time::now();
+      odom_tf_msg.header.frame_id = odom_frame_;
+      odom_tf_msg.child_frame_id = robot_base_frame_;
+      odom_tf_msg.transform = tf2::toMsg(odom_tf_);
+
+      tf_broadcaster_.sendTransform(odom_tf_msg);
     }
 
     void odomCallback(const nav_msgs::Odometry &odom)
     {
-      odom_ = odom;
+      tf2::fromMsg(odom.pose.pose, odom_tf_);
+    }
+
+    bool correctRobotPosition(tsuten_msgs::CorrectRobotPositionRequest &req,
+                              tsuten_msgs::CorrectRobotPositionResponse &res)
+    {
+      tf2::Vector3 new_robot_position;
+      tf2::fromMsg(req.robot_position, new_robot_position);
+      tf2::Transform new_robot_position_tf(tf2::Quaternion::getIdentity(), new_robot_position);
+
+      map_tf_ = new_robot_position_tf * odom_tf_.inverse();
+
+      return true;
     }
 
     ros::Subscriber odom_sub_;
 
     ros::Timer timer_;
 
-    nav_msgs::Odometry odom_;
+    ros::ServiceServer correct_robot_position_service_server_;
+
+    tf2::Transform map_tf_;
+    tf2::Transform odom_tf_;
 
     tf2_ros::TransformBroadcaster tf_broadcaster_;
+
+    std::string global_frame_;
+    std::string odom_frame_;
+    std::string robot_base_frame_;
 
     std::string odom_topic_;
 
