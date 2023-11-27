3a305c94882b6e994291463ba466eb0129f44eb2
mitukou1109 <mitukou1109@gmail.com>
2023-11-27 06:03:45 +0000

    wip

---
+++ tsuten_description/launch/spawn_robot.launch.py
@@ -0,0 +1,14 @@
+from launch import LaunchDescription
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    LaunchConfiguration,
+    PathJoinSubstitution,
+)
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    pass
